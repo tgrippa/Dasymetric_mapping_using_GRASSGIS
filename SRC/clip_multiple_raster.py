a2b21069efc56f0ce75c807ab255a181b5fe8e35
tais <tgrippa@ulb.ac.be>
2018-11-11 07:58:49 +0100

    move check of add-on in the function to avoid error when importing the function in the main script

--- SRC/clip_multiple_raster.py
+++ SRC/clip_multiple_raster.py
@@ -5,7 +5,6 @@ import multiprocessing
 from multiprocessing import Pool
 from functools import partial
 
-
 def clip(name):
     """ Define a short function for the clip operation """
     global suffix, o, r
@@ -28,6 +27,13 @@ def clip_multiple_raster(raster_name_list, output_suffix='clip', overwrite=False
     o = overwrite
     r = resample
     suffix = output_suffix
+
+    # Check if i.segment.stats is well installed
+    if not gscript.find_program('r.clip', '--help'):
+        message = _("You first need to install the addon r.clip.\n")
+        message += _(" You can install the addon with 'g.extension r.clip'")
+        gscript.fatal(message)
+
     # Clip the rasters in multiprocessing pool of jobs
     p = Pool(n_jobs)
     output = p.map(clip, raster_name_list)  # Launch the processes for as many items in the list (if function with a return, the returned results are ordered thanks to 'map' function)
