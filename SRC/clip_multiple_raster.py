025553ceb21a2bb591518f5566d82e6bca497a70
tais <tgrippa@ulb.ac.be>
2018-11-11 07:38:02 +0100

    update script to allow resampling of clipped rasters

--- SRC/clip_multiple_raster.py
+++ SRC/clip_multiple_raster.py
@@ -8,21 +8,25 @@ from functools import partial
 
 def clip(name):
     """ Define a short function for the clip operation """
-    global suffix, o
+    global suffix, o, r
     try:
-        gscript.run_command('r.clip', overwrite=o, input=name, output='%s_%s'%(name,suffix))
-        return "'%s' has been cliped"%name
+        if r:
+            gscript.run_command('r.clip', flags='r', overwrite=o, input=name, output='%s_%s'%(name,suffix))  # With resampling
+        else:
+            gscript.run_command('r.clip', overwrite=o, input=name, output='%s_%s'%(name,suffix))  # Without resampling
+        return "'%s' has been cliped."%name
     except:
-        return "ERROR: '%s' has not been cliped."%name
+        return "ERROR: '%s' has not been cliped. Please check for problem."%name
 
 
-def clip_multiple_raster(raster_name_list, output_suffix='clip', overwrite=False, n_jobs=2):
+def clip_multiple_raster(raster_name_list, output_suffix='clip', overwrite=False, resample=False, n_jobs=2):
     """ Define the function to clip a collection of rasters.
     Please be carefull that the clip will be based on region extend and pixels under MASK will be null.
     Please take care of well defining the computational region and a MASK if desired before calling the function.
     """
-    global suffix, o
+    global suffix, o, r
     o = overwrite
+    r = resample
     suffix = output_suffix
     # Clip the rasters in multiprocessing pool of jobs
     p = Pool(n_jobs)
